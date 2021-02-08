333901be827fe79b14b00eda101ef8c07c0c78a2
warlley01 <warlleyjhones@gmail.com>
2021-02-08 12:50:52 -0500

    import

---
+++ javascript.js
@@ -0,0 +1,283 @@
+
+pergunta1 = {
+    imagem : "https://media.giphy.com/media/SHihPPBYgmwNi/giphy.gif", 
+    pergunta : "O COVID-19 é transmitido pelo ar?", 
+    alternativas : ["Sim" , "Não"] ,
+    resposta : "A transmissão acontece de uma pessoa doente para outra ou por contato próximo",
+    indiceCorreto : 1
+}        
+
+pergunta2 = {
+    imagem : "https://media.giphy.com/media/h26R1JMxiqYpwp0rkF/giphy.gif", 
+    pergunta : "Somente pessoas infectadas devem usar Máscaras?", 
+    alternativas : ["Somente Infectados" , "Todas as pessoas devem usar máscara", "Os que estão no grupo de risco"],
+    resposta : "Dados científicos recentes constatam que a transmissão da COVID-19 pode ocorrer" +
+     " mesmo antes do indivíduo apresentar os primeiros sinais e sintomas. Por esse motivo," 
+     + " o Ministério da Saúde passou a recomendar o uso de máscaras faciais para todos",
+    indiceCorreto : 1
+}
+
+pergunta3 = {
+    imagem : "https://media.giphy.com/media/y3QOvy7xxMwKI/giphy.gif", 
+    pergunta : "Quem está mais vulnerável à COVID-19?", 
+    alternativas : ["Adultos" , "Jovens", "Crianças", "Idosos"] ,
+    resposta : "Pessoas idosas e pessoas com condições médicas pré-existentes são os mais vulneráveis",
+    indiceCorreto : 3
+}       
+
+pergunta4 = {
+    imagem : "https://media.giphy.com/media/QBd2kLB5qDmysEXre9/giphy.gif", 
+    pergunta : "Qual é o tempo de incubação do novo coronavírus?", 
+    alternativas : ["1 dia" , "De 1 à 2 semanas", "1 mês", "Algumas Horas"] ,
+    resposta : "O período de incubação se refere ao tempo entre a infecção do ser humano pelo vírus e o início" +
+    " dos sintomas da doença. De acordo com a Organização Mundial da Saúde (OMS)," + 
+    " no caso da Covid-19 esse intervalo varia de 1 a 14 dias, geralmente ficando em torno de 5 dias.",
+    indiceCorreto : 1
+}        
+
+pergunta5 = {
+    imagem : "https://media.giphy.com/media/l2JejHjYm3wb2GSC4/giphy.gif", 
+    pergunta : "Pessoas que usam medicação para doenças pré-existentes," +
+    " se infectadas pelo novo coronavírus, devem interromper ou modificar seus tratamentos?", 
+    alternativas : ["Sim" , "Não"] ,
+    resposta : "Especialistas alertam que pessoas com doenças pré-existentes e/ou comorbidades," +
+    " antes de interromper qualquer medicação ou tratamento, devem consultar um profissional médico.",
+    indiceCorreto : 1
+}      
+
+pergunta6 = {
+    imagem : "https://media.giphy.com/media/lvUUzGUU2w7yeCxQ60/giphy.gif", 
+    pergunta : "Fumantes correm maior risco de infecção pelo novo coronavírus?", 
+    alternativas : ["Sim" , "Não"] ,
+    resposta : "Fumar danifica os pulmões e outras partes do corpo. Além disso," +
+    " aumenta o risco de desenvolvimento da forma grave da Covid-19.",
+    indiceCorreto : 0
+}      
+
+pergunta7 = {
+    imagem : "https://media.giphy.com/media/adJZYLosk8afS/giphy.gif", 
+    pergunta : "Em quanto tempo uma pessoa infectada de forma leve está curada da doença?", 
+    alternativas : ["De 10 à 14 dias" , "De 2 à 5 Dias", "3 Meses"] ,
+    resposta : "Em média, as pessoas começam a melhorar a partir do sétimo dia e ficam boas em cerca de 10 dias," + 
+    " devendo ficar isoladas no mínimo por até 14 dias após o início dos sintomas.",
+    indiceCorreto : 0
+}      
+
+pergunta8 = {
+    imagem : "https://media.giphy.com/media/9GJ1sNJrEi5uB0nZ6S/giphy.gif", 
+    pergunta : "Como é feito o diagnóstico do coronavírus?", 
+    alternativas : ["Amostra de Sangue" , "Amostra de Urina", "Amostras Respiratórias"] ,
+    resposta : "A partir da coleta de amostras respiratórias de pacientes considerados suspeitos," + 
+    "são realizados testes baseados em técnicas de biologia molecular",
+    indiceCorreto : 2
+}    
+
+pergunta9 = {
+    imagem : "https://media.giphy.com/media/pUgwLxmGW3S7K/giphy.gif", 
+    pergunta : "A pessoa pode transmitir o coronavírus mesmo sem apresentar sintoma?", 
+    alternativas : ["Sim" , "Não"] ,
+    resposta : "Pelos dados apresentados até momento, o período de incubação do vírus pode" +
+    " variar de dois a 14 dias.",
+    indiceCorreto : 0
+}    
+
+pergunta10 = {
+    imagem : "https://media.giphy.com/media/3s8UeJsWzSNXV4WiA2/giphy.gif", 
+    pergunta : "Qual é a orientação para viajantes que retornaram de viagem internacional ou fizeram viagem pelo Brasil?", 
+    alternativas : ["Consultar um médico" , "Não fazer nada", "Isolamento por 7 dias após desembarque"] ,
+    resposta : "Se você voltou de viagem internacional, recomenda-se isolamento domiciliar voluntário" +
+    " por 7 dias após o desembarque, mesmo que não tenha apresentado os sintomas",
+    indiceCorreto : 2
+}    
+
+pergunta11 = {
+    imagem : "https://media.giphy.com/media/Zx8MFmqOGdXHO/giphy.gif", 
+    pergunta : "É possível se contaminar por meio de aperto de mãos ou com beijos no rosto?", 
+    alternativas : ["Sim" , "Não"] ,
+    resposta : "É recomendável evitar, ao menos temporariamente, esse tipo de contato físico," + 
+    " uma vez que pessoas assintomáticas também são capazes de transmitir a doença.",
+    indiceCorreto : 0
+}    
+
+pergunta12 = {
+    imagem : "https://media.giphy.com/media/7MDQcbu0iU7Di/giphy.gif", 
+    pergunta : "Como posso fortalecer minha imunidade para me proteger do novo coronavírus?", 
+    alternativas : ["Beber" , "Fumar", "Alimentação Saudavel e Exercícios Físicos"] ,
+    resposta : "É importante manter uma alimentação saudável, praticar exercícios físicos, não fumar," + 
+    " não usar drogas e não consumir bebidas alcoólicas," + 
+    " priorizando fazer coisas que lhe deem prazer (mas sempre respeitando a recomendação de não sair de casa).",
+    indiceCorreto : 2
+}
+
+pergunta13 = {
+    imagem : "https://media.giphy.com/media/LPqyrcoFuaAIRSo07t/giphy.gif", 
+    pergunta : "Máscaras caseiras podem ajudar na prevenção contra o novo coronavírus?", 
+    alternativas : ["Não" , "Sim"] ,
+    resposta : "A partir de abril de 2020, o Ministério da Saúde passou" + 
+    " a recomendar à população brasileira o uso de máscaras e a orientar a produção de máscaras caseiras",
+    indiceCorreto : 1
+}    
+
+pergunta14 = {
+    imagem : "https://media.giphy.com/media/PP3rTELcypEDULR0Dv/giphy.gif", 
+    pergunta : "Quais as orientações para higienização de alimentos para prevenir a Covid-19?", 
+    alternativas : ["Lavar com água é suficiente" , "Lavar embalagens com Água e sabão, e verduras com água sanitária", "Não é necessário higienizar"] ,
+    resposta : "Assim que os alimentos chegam em casa, é importante fazer a higienização de cada item." +
+    " Se o alimento for embalado, a lavagem com água e sabão ou desifecção com álcool a 70% é suficiente." +
+    " Em relação a verduras, legumes e frutas, o uso de água sanitária é recomendado, na proporção de uma colher" +
+    " de sopa para cada litro de água.",
+    indiceCorreto : 1
+}    
+
+pergunta15 = {
+    imagem : "https://media.giphy.com/media/26xBI73gWquCBBCDe/giphy.gif", 
+    pergunta : "Uma pessoa que teve coronavírus e se recuperou totalmente ainda pode transmitir o vírus?", 
+    alternativas : ["Se confirmado que se recuperou não existe mais risco" , "Após recuperação o virus ainda pode ser transmitido"] ,
+    resposta : "Nos casos leves, em que a pessoa infectada passou por isolamento domiciliar de 14 dias e" +
+    " não apresenta mais sintomas e, nos casos graves, após o exame laboratorial indicar que não" +
+    " há mais vírus no organismo, considera-se que não existe risco de contágio a partir do organismo desse indivíduo.",
+    indiceCorreto : 0
+}   
+
+
+// indice do arrayObjetos
+obj = 0
+
+function executar(){
+
+    // array com todas as perguntas
+    arrayObjetos = [pergunta1, pergunta2, pergunta3, pergunta4, pergunta5, pergunta6, pergunta7, 
+    pergunta8, pergunta9, pergunta10, pergunta11, pergunta12, pergunta13, pergunta14, pergunta15]    
+
+    var img = document.createElement("img")
+    img.setAttribute("src", arrayObjetos[obj].imagem)
+
+    var question = document.createElement("h4")
+    question.textContent = arrayObjetos[obj].pergunta
+    
+    for(i = 0; i < arrayObjetos[obj].alternativas.length; i++){
+
+        var button = document.createElement("button")       
+        button.textContent = arrayObjetos[obj].alternativas[i]
+        button.value = arrayObjetos[obj].alternativas[i]
+        button.setAttribute("onclick", "verificar(value)")    
+
+        var divImg = document.getElementById("divImg")
+        divImg.appendChild(img)
+        var divPergunta = document.getElementById("divPergunta")
+        divPergunta.appendChild(question)
+        var divOptions = document.getElementById("divOptions")
+        divOptions.appendChild(button)
+           
+        
+    }  
+}
+
+
+// Executar Ao carregar a pagina
+function comecar(){    
+    document.getElementById("divComecar").innerHTML = ""
+    executar()   
+}
+
+// Pontos por acerto
+var pontos = 0;
+
+// função fora da funçao functionQuiz
+function verificar(valor){
+    if(valor === arrayObjetos[obj].alternativas[arrayObjetos[obj].indiceCorreto]){       
+      //  console.log(arrayObjetos[obj].resposta) 
+        pontos++     
+      //  console.log(pontos)
+      //  alert("Correto" )   
+    // lIMPAR
+        document.getElementById("divImg").innerHTML = ""
+        document.getElementById("divPergunta").innerHTML = ""
+        document.getElementById("divOptions").innerHTML = ""
+    // Imagem Resposta Correta  
+        var imgCorrect = document.createElement("img")
+        imgCorrect.setAttribute("src", "https://i.pinimg.com/originals/06/ae/07/06ae072fb343a704ee80c2c55d2da80a.gif")  
+        document.getElementById("divImg").appendChild(imgCorrect)  
+    
+    // Acertou
+        var divQuest = document.getElementById("divPergunta")
+        var correto = document.createElement("h2")
+        correto.classList.add("acertou")
+        correto.textContent = "Você Acertou"
+        divQuest.appendChild(correto)
+
+
+    // Botão Continuar
+        var next = document.createElement("button")
+        next.textContent = "Continuar"
+        next.setAttribute("onclick", "proximo()")
+        document.getElementById("divOptions").appendChild(next)
+    // Detalhes da Resposta
+        var divDetalhes = document.getElementById("detalhes")
+        var info = document.createElement("p")
+        info.textContent = arrayObjetos[obj].resposta
+        divDetalhes.appendChild(info)
+        
+
+    } else {        
+    // alert("Resposta Errada")   
+        console.log(arrayObjetos[obj].resposta)  
+        
+        document.getElementById("divImg").innerHTML = ""
+        document.getElementById("divPergunta").innerHTML = ""
+        document.getElementById("divOptions").innerHTML = ""
+    // Imagem Resposta Correta  
+        var imgCorrect = document.createElement("img")
+        imgCorrect.setAttribute("src", "https://media.giphy.com/media/pjFF8YLW996aXqFHAu/giphy.gif")  
+        document.getElementById("divImg").appendChild(imgCorrect) 
+        
+    // Acertou
+        var divQuest = document.getElementById("divPergunta")
+        var correto = document.createElement("h2")
+        correto.classList.add("errou")
+        correto.textContent = "Você Errou"
+        divQuest.appendChild(correto)
+
+    // Botão Continuar
+        var next = document.createElement("button")
+        next.textContent = "Continuar"
+        next.setAttribute("onclick", "proximo()")
+        document.getElementById("divOptions").appendChild(next)
+        
+    // Detalhes da Resposta
+        var divDetalhes = document.getElementById("detalhes")
+        var info = document.createElement("p")
+        info.textContent = arrayObjetos[obj].resposta
+        divDetalhes.appendChild(info)
+    }
+      
+}
+
+function proximo(){
+    //  limpar div
+        document.getElementById("divImg").innerHTML= ""
+        document.getElementById("divPergunta").innerHTML = ""
+        document.getElementById("divOptions").innerHTML = ""
+        document.getElementById("detalhes").innerHTML = ""
+        
+        if(obj < 14){
+            // Proxima pergunta        
+            obj++
+            // Executar função com a proxima pergunta
+            executar()
+        } else {
+            // Quando acabar todas as perguntas o else é executado
+            var divPontuacao = document.getElementById("pontuacao")
+            var divRe = document.getElementById("divReplay")
+            var pont = document.createElement("h2") 
+            pont.classList.add("mensagem")           
+            var link = document.createElement("a")
+            link.textContent = " Recomeçar"
+            link.setAttribute("href", "index.html")
+            pont.textContent = "Você Acertou: " + pontos + " Perguntas do total de (15) "
+            
+            divPontuacao.appendChild(pont)
+            pont.appendChild(divRe)
+            divRe.appendChild(link)
+        }        
+}
